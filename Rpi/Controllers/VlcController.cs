95e2e13e0c88c47d83cd44cddcb24600e7130ea9
cores-system <dev@sped-mail.ru>
2019-04-30 13:38:16 +0300

    Добавьте файлы проекта.

---
+++ Rpi/Controllers/VlcController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rpi.Controllers
+{
+    public class VlcController : ControllerBase
+    {
+        [Route("vlc/play")]
+        public ActionResult<string> Play(string uri)
+        {
+            try
+            {
+                var processInfo = new ProcessStartInfo();
+                processInfo.UseShellExecute = false;
+                processInfo.RedirectStandardOutput = true;
+                processInfo.FileName = "/bin/bash";
+                processInfo.Arguments = $" -c \"killall vlc; export DISPLAY=:0; vlc --fullscreen {uri}\"";
+                var process = Process.Start(processInfo);
+
+                return uri;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ Rpi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Rpi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(op => op.Listen(IPAddress.Any, 60817));
+    }
+}
